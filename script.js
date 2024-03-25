bc5d7f7d1a8259754ca9e72c8071d32a4ebfde22
matheus <matheusmuniz.office@gmail.com>
2024-03-25 19:43:33 -0300

    3

---
+++ script.js
@@ -0,0 +1,21 @@
+function abrirlogin()   {
+    document.getElementById('modal').style.top = "0%";
+}
+function fecharlogin()   {
+    document.getElementById('modal').style.top = "-100%";
+}
+
+function abrirlogin2()   {
+    document.getElementById('segundo').style.top = "17.5%";
+}
+function fecharlogin2()   {
+    document.getElementById('segundo').style.top = "-100%";
+}
+
+function abrirCadastro() {
+    document.getElementById('terceiro').style.top = "17.5%"; 
+}
+
+function fecharCadastro() {
+    document.getElementById('terceiro').style.top = "-100%"; 
+}
