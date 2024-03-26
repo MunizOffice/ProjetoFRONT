974ccb38efecd7e11e7b0eecf2c09d6fbfd94b11
matheus <matheusmuniz.office@gmail.com>
2024-03-25 21:56:11 -0300

    7

--- script.js
+++ script.js
@@ -56,4 +56,61 @@ function javascript(){
             alert(`erro: ${error.message}`);
         }
     })
+
+    function Resultado(lat, long) {
+
+        fetch(`${api.base}weather?lat=${lat}&lon=${long}&lang=${api.lang}&units=${api.units}&APPID=${api.key}`)
+            .then(res => {
+                return res.json();
+            })
+            .catch(error => {
+                alert(error.message)
+            })
+            .then(res => {
+                displayResultado(res)
+            });
+    }
+
+    
+    pesquisa_button.addEventListener('click', function() {
+        buscaResultado(pesquisa.value)
+    })
+
+    pesquisa.addEventListener('keypress', enter)
+    function enter(event) {
+        key = event.keyCode
+        
+        if (key === 13) {
+            buscaResultado(pesquisa.value)
+        }
+    }
+
+    function buscaResultado(cidade) {
+
+        fetch(`${api.base}weather?q=${cidade}&lang=${api.lang}&units=${api.units}&APPID=${api.key}`)
+            })
+            .catch(error => {
+                alert(error.message)
+            })
+            .then(res => {
+                displayResultado(res)
+            });
+    }
+
+}
+
+const email = document.getElementById("email");
+const password = document.getElementById("password");
+const form = document.querySelector("form");
+
+const email2 = document.getElementById("email2");
+const password2 = document.getElementById("password2");
+const name2 = document.getElementById("nom");
+const foto = document.getElementById("foto");
+
+function cadastrarDados()   {
+    dado1.textContent = name2.value;
+    dado2.textContent = email2.value;
+    dado3.textContent = password2.value;
+    perfil2.document = foto.value
 }
