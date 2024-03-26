bec84fd08edf3dd1f190db7f596b1873263159d4
matheus <matheusmuniz.office@gmail.com>
2024-03-25 21:21:53 -0300

    4

--- script.js
+++ script.js
@@ -12,10 +12,31 @@ function fecharlogin2()   {
     document.getElementById('segundo').style.top = "-100%";
 }
 
-function abrirCadastro() {
-    document.getElementById('terceiro').style.top = "17.5%"; 
+const api = {
+    key: "92c9e9841858f7442a67c01e9f7a13d2",
+    base: "https://api.openweathermap.org/data/2.5/",
+    lang: "pt_br",
+    units: "metric",
 }
 
-function fecharCadastro() {
-    document.getElementById('terceiro').style.top = "-100%"; 
-}
+const pesquisa = document.querySelector('.form-control');
+
+const pesquisa_button = document.querySelector('.btn');
+
+const cidade = document.querySelector('.cidade');
+
+const date = document.querySelector('.date');
+
+const weather_t = document.querySelector('.ceu');
+
+const tempo_img = document.querySelector('.tempo-img');
+
+const tempo_temp = document.querySelector('.tempo-temp');
+
+const temp_number = document.querySelector('.tempo-temp div');
+
+const temp_unit = document.querySelector('.tempo-temp span');
+
+const max_min = document.querySelector('.max-min');
+
+const humidade = document.querySelector('.humidade');
